2b3e9a763ce453d7999be03631c22ee59b66a1fe
Nktpamungkas <nktpamungkas28@gmail.com>
2026-01-26 10:12:01 +0700

    tambahkan schema saat hapus

--- pages/ci_hapus.php
+++ pages/ci_hapus.php
@@ -1,6 +1,6 @@
 <?php
     $modal_id=$_GET['id'];
-    $modal1=sqlsrv_query($con,"DELETE FROM tbl_exim_cim WHERE id='$modal_id' ");
+    $modal1=sqlsrv_query($con,"DELETE FROM db_qc.tbl_exim_cim WHERE id='$modal_id' ");
     if ($modal1) {
         echo "<script>window.location='?p=Commercial-Invoice-Manual';</script>";
     } else {
